692ee9b3f7d9a1a5e4f50439fc7abc9685b28459
sirangula subrahmanyeswari <ssirangula@gmail.com>
2021-07-10 14:04:15 +0000

    First Commit

--- script.js
+++ script.js
@@ -1,6 +1,16 @@
 function compute()
 {
-    p = document.getElementById("principal").value;
-    
+    var prinicipal = document.getElementById("principal").value;
+    var rate= document.getElementById("rate"),value;
+    var years = document.getElementById("years"),value;
+    var Interest = prinicipal *rate*years /100;
+    var year= new Date().getFullYear()+parseInt(years);
+    document.getElementById("result").innerHTML="If you deposit "
+    +prinicipal+ " at an interset of" + rate+"%.You will receive an amount of"+
+    Interest+"," +" in the year "+year;
 }
-        
+function updateRate()
+{
+    var rateval= document.getElementById("rate").value;
+    document.getElementById("rate_val").innerText=rateval;
+}
